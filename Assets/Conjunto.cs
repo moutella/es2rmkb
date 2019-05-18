47ec27472edcba0644f51aad2316a2b37c4a038e
Luan Simões <luansimoes@id.uff.br>
2019-05-18 14:01:17 -0300

    Resolvendo erros de sintaxe e criando construtor de Conjunto

--- Assets/Conjunto.cs
+++ Assets/Conjunto.cs
@@ -1,18 +1,21 @@
 ﻿using System;
 using System.Collections;
-using Peca;
 
 public class Conjunto
 {
     private ArrayList pecas;
-    //Contains
 
+
+    public Conjunto(){
+        pecas = new ArrayList();
+    }
+    
     public void inserePeca(Peca p){
         pecas.Add(p);
     }
 
     public void removePeca(Peca p){
-        pecas.Remove(Peca p);
+        pecas.Remove(p);
     }
 
     public void removePeca(int i){
