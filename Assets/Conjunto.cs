b6a0bc2cd06396ade8fa64c92910fcc1b5729702
Luan Simões <luansimoes@id.uff.br>
2019-05-18 13:49:24 -0300

    adding class Conjunto

---
+++ Assets/Conjunto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using Peca;
+
+public class Conjunto
+{
+    private ArrayList pecas;
+    //Contains
+
+    public void inserePeca(Peca p){
+        pecas.Add(p);
+    }
+
+    public void removePeca(Peca p){
+        pecas.Remove(Peca p);
+    }
+
+    public void removePeca(int i){
+        if(i<pecas.Count){pecas.RemoveAt(i);}
+    }
+}
