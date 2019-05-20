1931a2a98e0cbcbe3e4c1a678d78b268de729219
Luan Simões <luansimoes@id.uff.br>
2019-05-20 19:11:43 -0300

    Classe Jogador adicionada

---
+++ Assets/Scripts/base_logica/Jogador.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class Jogador
+{
+    private MaoUsuario mao;
+    private bool primJogada;
+    private ArrayList jogadaAtual;
+
+    public Jogador(){
+        mao = new MaoUsuario();
+        primJogada = true;
+        jogadaAtual = new ArrayList();
+    }
+
+}
