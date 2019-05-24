ddebbb21b090b3e5afcf6e9c2a9932394b054bc8
Lucas Bicalho <lucassbcosta@hotmail.com>
2019-05-23 22:06:55 -0300

    Inicializando a busca de jogadas possiveis pela IA

--- Assets/Scripts/base_logica/IA.cs
+++ Assets/Scripts/base_logica/IA.cs
@@ -5,24 +5,80 @@ using UnityEngine;
 public class IA
 {
 
-	private bool primeiraJogada;
-	private MaoUsuario mao;
-
-	public IA(MaoUsuario m)
+	class ComparadorPorCor : IComparer
 	{
-		primeiraJogada = true;
-		this.mao = m;
+		int IComparer.Compare(Peca x, Peca y)
+		{
+			if (x.getCodigoCor() < y.getCodigoCor()) { return -1; }
+			if (x.getCodigoCor() > y.getCodigoCor()) { return 1; }
+			if (x.getValor() < y.getValor()) { return -1; }
+			if (x.getValor() >= y.getValor()) { return 1; }
+		}
 	}
 
-	public bool getPrimeiraJogada() { return this.primeiraJogada; }
-	public void setPrimeiraJogada(bool pj) { this.primeiraJogada = pj; }
+
+	class ComparadorPorValor : IComparer
+	{
+		int IComparer.Compare(Peca x, Peca y)
+		{
+			if (x.getValor() < y.getValor()) { return -1; }
+			if (x.getValor() > y.getValor()) { return 1; }
+			if (x.getCodigoCor() < y.getCodigoCor()) { return -1; }
+			if (x.getCodigoCor() >= y.getCodigoCor()) { return 1; }
+		}
+	}
+
+
+	private MaoUsuario mao;
+
+	public IA(MaoUsuario m) { this.mao = m; }
 
 	public MaoUsuario getMao() { return this.mao; }
 	public void setMao(MaoUsuario m) { this.mao = m; }
 
+
+	public ArrayList procuraConjunto(bool primeiraJogada)
+	{
+		ArrayList pecas = this.mao.getPecas();
+		pecas.Sort(new ComparadorPorCor());
+		Peca inicio;
+		Peca anterior;
+		int soma = 0;
+		ArrayList conjunto = new ArrayList();
+		foreach (Peca atual in pecas)
+		{
+			if (inicio = null)
+			{
+				inicio = atual;
+				anterior = atual;
+				soma += atual.getValor();
+				conjunto.Add(atual);
+			}
+			else
+			{
+				if (atual.getValor() - anterior.getValor() = 1 && atual.getCodigoCor() = anterior.getCodigoCor())
+				{
+					anterior = atual;
+					soma += atual.getValor();
+					conjunto.Add(atual);
+				}
+				else if (conjunto.Count >= 3 && ((primeiraJogada && soma >= 30) || (!primeiraJogada)))
+				{
+					return conjunto;
+				}
+				else
+				{
+					conjunto.Clear();
+					soma = conjunto.getValor();
+				}
+			}
+		}
+	}
+
+
 	public void jogada()
 	{
-		if (primeiraJogada)
+		if (this.mao.getPrimeiraJogada())
 		{
 			/*
 			procura Conjunto >= 30 pontos
