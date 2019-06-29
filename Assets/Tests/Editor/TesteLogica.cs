04c2f2981a38ec3b31c875acd42fdb577dcc19c8
lucasalexandre-cc <lucasalexandre.s.cc@gmail.com>
2019-06-29 19:40:11 -0300

    Create test folder and first unit test

---
+++ Assets/Tests/Editor/TesteLogica.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TesteLogica
+{
+    
+    [Test]
+    public void ValidateSequence()
+    {
+        //Conjunto conjunto = new Conjunto();
+        Assert.AreEqual(1, 1);
+    }
+}
